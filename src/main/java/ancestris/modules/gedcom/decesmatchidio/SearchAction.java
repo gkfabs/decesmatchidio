a61bd7c31854f18ca4a1169d4e8f7cdf2c65fc24
Fabien Carrion <fabien@carrion.mx>
2025-10-23 08:24:17 -0600

    Update editors view after updating the underlying data

--- src/main/java/ancestris/modules/gedcom/decesmatchidio/SearchAction.java
+++ src/main/java/ancestris/modules/gedcom/decesmatchidio/SearchAction.java
@@ -14,13 +14,17 @@ package ancestris.modules.gedcom.decesmatchidio;
 import ancestris.core.actions.AbstractAncestrisContextAction;
 import ancestris.util.swing.DialogManager;
 import genj.gedcom.Entity;
+import genj.gedcom.Gedcom;
+import genj.gedcom.GedcomException;
 import genj.gedcom.Indi;
 import genj.gedcom.Property;
 import genj.gedcom.TagPath;
+import genj.gedcom.UnitOfWork;
 import java.awt.event.ActionEvent;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.logging.Logger;
+import org.openide.util.Exceptions;
 import org.openide.util.LookupEvent;
 import org.openide.util.NbBundle;
 
@@ -65,39 +69,48 @@ public class SearchAction extends AbstractAncestrisContextAction {
         DialogManager.ADialog dialog = DialogManager.create(
                 NbBundle.getMessage((Class) SearchAction.class, "SearchPanel.dialogManager.title"), searchPanel);
         if (dialog.show() == DialogManager.ADialog.OK_OPTION) {
-            final DecesMatchId decesMatchIdFound = searchPanel.getSelected();
-            if (decesMatchIdFound == null) {
-                return;
-            }
-            final Indi indi = (Indi) this.entity;
-            indi.setName(decesMatchIdFound.getFirstName(), decesMatchIdFound.getLastName());
-            switch (decesMatchIdFound.getSex().toString()) {
-                case "M": {
-                    indi.setSex(1);
-                    break;
-                }
-                case "F": {
-                    indi.setSex(2);
-                    break;
-                }
-            }
-            final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
-            final String birthPlace = ", " + decesMatchIdFound.getBirthCity() + ", , "
-                    + decesMatchIdFound.getBirthDepartment() + ", , " + decesMatchIdFound.getBirthCountry();
-            indi.setValue(new TagPath("INDI:BIRT:DATE"), decesMatchIdFound.getBirthLocalDate().format(formatter));
-            indi.setValue(new TagPath("INDI:BIRT:PLAC"), birthPlace);
-            final String deathPlace = ", " + decesMatchIdFound.getDeathCity() + ", , "
-                    + decesMatchIdFound.getDeathDepartment() + ", , " + decesMatchIdFound.getDeathCountry();
-            indi.setValue(new TagPath("INDI:DEAT:DATE"), decesMatchIdFound.getDeathLocalDate().format(formatter));
-            indi.setValue(new TagPath("INDI:DEAT:PLAC"), deathPlace);
-            final String description = "DecesMatchId: certificate: " + decesMatchIdFound.getDeathCertificateId()
-                    + ", source: " + decesMatchIdFound.getSource() + ", source line: "
-                    + decesMatchIdFound.getSourceLine();
-            Property death = indi.getProperty(new TagPath("INDI:DEAT"));
-            if (death == null) {
-                death = indi.setValue(new TagPath("INDI:DEAT"), "");
-            }
-            death.addSimpleProperty("SOUR", description, -1);
+            try {
+                this.entity.getGedcom().doUnitOfWork(new UnitOfWork() {
+                            @Override
+                            public void perform(Gedcom gedcom) throws GedcomException {
+                                final DecesMatchId decesMatchIdFound = searchPanel.getSelected();
+                                if (decesMatchIdFound == null) {
+                                    return;
+                                }
+                                final Indi indi = (Indi) entity;
+                                indi.setName(decesMatchIdFound.getFirstName(), decesMatchIdFound.getLastName());
+                                switch (decesMatchIdFound.getSex().toString()) {
+                                case "M": {
+                                    indi.setSex(1);
+                                    break;
+                                }
+                                case "F": {
+                                    indi.setSex(2);
+                                    break;
+                                }
+                                }
+                                final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+                                final String birthPlace = ", " + decesMatchIdFound.getBirthCity() + ", , "
+                                    + decesMatchIdFound.getBirthDepartment() + ", , " + decesMatchIdFound.getBirthCountry();
+                                indi.setValue(new TagPath("INDI:BIRT:DATE"), decesMatchIdFound.getBirthLocalDate().format(formatter));
+                                indi.setValue(new TagPath("INDI:BIRT:PLAC"), birthPlace);
+                                final String deathPlace = ", " + decesMatchIdFound.getDeathCity() + ", , "
+                                    + decesMatchIdFound.getDeathDepartment() + ", , " + decesMatchIdFound.getDeathCountry();
+                                indi.setValue(new TagPath("INDI:DEAT:DATE"), decesMatchIdFound.getDeathLocalDate().format(formatter));
+                                indi.setValue(new TagPath("INDI:DEAT:PLAC"), deathPlace);
+                                final String description = "DecesMatchId: certificate: " + decesMatchIdFound.getDeathCertificateId()
+                                    + ", source: " + decesMatchIdFound.getSource() + ", source line: "
+                                    + decesMatchIdFound.getSourceLine();
+                                Property death = indi.getProperty(new TagPath("INDI:DEAT"));
+                                if (death == null) {
+                                    death = indi.setValue(new TagPath("INDI:DEAT"), "");
+                                }
+                                death.addSimpleProperty("SOUR", description, -1);
+                            }
+                        });
+                    } catch (GedcomException ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
         }
     }
 }
